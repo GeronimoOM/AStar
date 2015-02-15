4ff83c6c902d7ba2f09ed3512049ad854f942258
Vitalik <mogolivskiy@gmail.com>
2015-02-15 11:12:39 +0200

    add one comment to main method

--- src/Main.java
+++ src/Main.java
@@ -51,6 +51,7 @@ public class Main {
     }
 
 	public static void start(){
+		//I have added this line=)
     	try {
     		displayMode = new DisplayMode(1920, 1080);
     		Display.setDisplayMode(displayMode);
