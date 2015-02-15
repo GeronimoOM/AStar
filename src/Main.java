b4e9440e8681c12f030d5634fd58485041d3be5b
Vitalik <mogolivskiy@gmail.com>
2015-02-15 11:58:43 +0200

    change resolution to 960 t0 540

--- src/Main.java
+++ src/Main.java
@@ -53,7 +53,7 @@ public class Main {
 	public static void start(){
 		//I have added this line=)
     	try {
-    		displayMode = new DisplayMode(1920, 1080);
+    		displayMode = new DisplayMode(960, 540);
     		Display.setDisplayMode(displayMode);
     		Display.create();
     	} catch (LWJGLException e) {
